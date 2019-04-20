faeef14534fd9f95799e7b3445ccb3a695455b78
KarenHashimoto <carrie4021@gmail.com>
2019-04-20 16:51:47 +0900

    最新版！

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8"/>
     <title>camcierge★</title>
     <meta name="viewport" content="width=device-width; initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" />
+    <!-- <link rel="stylesheet" href="style.css" /> -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <!-- css -->
     <link rel="stylesheet" href="{{ asset('css/top.css') }}">
@@ -18,112 +18,129 @@
 </head>
 
 <body>
-    <div class="header">
-        <a href="index.html">camcierge</a>
-    </div>
-    <div class="tab">
-        <div class="tab_item">２時間</div>
-        <div class="tab_item">豪華</div>
-        <div class="tab_item">ゆったり</div>
-    </div>
-    <div class="top_var">キャンプ女子におすすめのキャンプ場</div>
-    <img src="" alt="" title="tent_mark">
-    <div class="recommend_camp">
-        <div class="nav_title"><a id="drive_eta">福岡から2時間以内のキャンプ場</a></div>
-        <div class="nav_title_img">
-            <img src="images/main-img.jpg" width= max alt="">
-        </div>
-        <div class="nav_textbox">キャンシェルジュオススメの福岡県から車で2時間のキャンプ場を紹介します</div>
+	<div class="header">
+		<a class="logo" href="index.html">camcierge</a>
+	</div>
+			<!-- <div class="nav">
+				<div class="nav1"><a href="#drive_eta"><i class="material-icons">drive_eta</i>2時間</a></div>
+				<div class="nav2"><a href="#pets"><i class="material-icons">pets</i>ペット</a></div>
+				<div class="nav3"><a href="#spa"><i class="material-icons">spa</i>ゆったり</a></div>
+			</div> -->
+		<!-- <div class="tabmenu">
+			<label>
+				<input name="tab" type="radio" checked>
+				<em><div class="nav1"><a href="#drive_eta"><i class="material-icons">drive_eta</i>2時間</a></div></em>
+				<span>TEXT1</span>
+			</label>
+			<label>
+				<input name="tab" type="radio">
+				<em><div class="nav2"><a href="#pets"><i class="material-icons">pets</i>ペット</a></div></em><span>TEXT2</span>
+			</label>
+			<label>
+				<input name="tab" type="radio">
+				<em><div class="nav3"><a href="#spa"><i class="material-icons">spa</i>ゆったり</a></div></em><span>TEXT3</span>
+			</label>
+			</div> -->
+	<div class="tab">
+		<div class="tab_item">２時間</div>
+		<div class="tab_item">豪華</div>
+		<div class="tab_item">ゆったり</div>
+	</div>
 
-        <div class="container">
+	<div class="top_var">キャンプ女子におすすめのキャンプ場</div>
 
-            <div class="card">
-                <div class="box2">
-                    <img src="icon.jpg" class="icon2">
-                    <div>
-                        <div class="title2">															
-                            <div class="camp_name">aaaaa</div>
-                            <!-- 仮でイメージ入力 -->
-                            ボイボイキャンプ場
-                        </div>
-                        <p class="name2">(大分/くじゅう高原)</p>
-                    </div>
-                </div>
-                <!-- <img src="sample.jpg" class="image2">
-                <div class="camp_pc"> -->
-                <img src="sample.jpg" alt=""  class="camp_img">
-                <!-- </div> -->
+	<div class="recommend_wrap">
 
-
-
-
-<div class="recommend1">
-
-
-
-<!-- 仮でイメージ入力 -->
-<a href="#" class="btn-gradient-radius">広々</a>
-<a href="#" class="btn-gradient-radius">星空</a>
-<a href="#" class="btn-gradient-radius">温泉</a>
+		<div class="recommend_head">
+			<img src="" alt="" title="tent_mark">
+			<div class="nav_title"><a id="drive_eta">福岡から2時間以内のキャンプ場</a></div>
+			<div class="nav_title_img">
+						<img src="images/main-img.jpg" width= max alt="">
+			</div>
+			<div class="nav_textbox">キャンシェルジュオススメの福岡県から車で2時間のキャンプ場を紹介します</div>
+		</div>
 
 
-<div class="recommend_img_1">
-    <img src="" alt="" title="recommend_img_1"></div>
-<div class="recommend_img_2"><img src="" alt=""
-title="recommend_img_2"></div>
-<div class="recommend_img_3"><img src="" alt=""
-title="recommend_img_3"></div>
-<div class="detail"><a href="/detail/1" class="btn-square-little-rich">キャンプ場詳細を見る</a></div>
-</div>
-</div>
+		<div class="container">
+
+			<div class="card">
+				<div class="box2">
+					<!-- <img src="icon.jpg" class="icon2"> -->
+					<div>
+						<div class="title2">
+							<div class="camp_id">丸丸丸
+
+                            </div>
+							<!-- 仮でイメージ入力 -->
+							ボイボイキャンプ場
+						</div>
+						<div class="camp_area">(大分/くじゅう高原)</div>
+					</div>
+				</div>
+				<!-- <img src="sample.jpg" class="image2">-->
+				<!-- <div class="camp_pc"> 
+				<img src="sample.jpg" alt=""  class="camp_img">
+				<img src="sample.jpg" alt=""  class="camp_img"> -->
 
 
+				<div class="camp_pc-a">
+					<div class="camp_pc">
+						<img class="camp_img"src="https://tabelog.com/imgview/original?id=r5061739933397">
+					</div>
+					<div class="camp_pc">
+						<img class="camp_img"src="https://tabelog.com/imgview/original?id=r5070939933449">
+					</div>
+				</div>
 
 
+				<div class="recommend1">
+
+					<!-- 仮でイメージ入力 -->
+					<div class="btn-a">
+						<a href="#" class="btn-gradient-radius">広々</a>
+						<a href="#" class="btn-gradient-radius">星空</a>
+						<a href="#" class="btn-gradient-radius">温泉</a>
+					</div>
+
+					<!-- <div class="recommend_img_1"><img src="" alt=""
+														title="recommend_img_1"></div>
+					<div class="recommend_img_2"><img src="" alt=""
+														title="recommend_img_2"></div>
+					<div class="recommend_img_3"><img src="" alt=""
+														title="recommend_img_3"></div> -->
+					<div class="detail"><a href="detail.html" class="btn-square-little-rich">キャンプ場詳細を見る</a></div>
+				</div>
+			</div>
+
+			<div class="recommend2">
+				<div class="camp_id">●●●</div>
+				<div class="camp_pc"><img src="" alt="" class="camp_img"></div>
+				<div class="recommend_img_1"><img src="" alt=""
+													title="recommend_img_1"></div>
+				<div class="recommend_img_2"><img src="" alt=""
+													title="recommend_img_2"></div>
+				<div class="recommend_img_3"><img src="" alt=""
+													title="recommend_img_3"></div>
+				<div class="detail"><a href="detail.html">キャンプ場詳細を見る</a></div>
+			</div>
+
+			<div class="recommend3">
+							<div class="camp_id">●●●</div>
+							<div class="camp_pc"><img src="" alt="" class="camp_img"></div>
+							<div class="recommend_img_1"><img src="" alt=""
+																title="recommend_img_1"></div>
+							<div class="recommend_img_2"><img src="" alt=""
+																title="recommend_img_2"></div>
+							<div class="recommend_img_3"><img src="" alt=""
+																title="recommend_img_3"></div>
+							<div class="detail"><a href="detail.html">キャンプ場詳細を見る</a></div>
+			</div>
+
+		</div>
+	</div>
 
 
-
-
-
-
-<div class="recommend2">
-<div class="camp_id">●●●</div>
-<div class="camp_pc"><img src="" alt="" class="camp_img"></div>
-<div class="recommend_img_1"><img src="" alt=""
-title="recommend_img_1"></div>
-<div class="recommend_img_2"><img src="" alt=""
-title="recommend_img_2"></div>
-<div class="recommend_img_3"><img src="" alt=""
-title="recommend_img_3"></div>
-<div class="detail"><a class="nav-link" href="{{-- action('InformationController@detail') --}}#contact">キャンプ場詳細を見る</a></div>
-</div>
-
-
-<div class="recommend3">
-<div class="camp_id">●●●</div>
-<div class="camp_pc"><img src="" alt="" class="camp_img"></div>
-<div class="recommend_img_1"><img src="" alt=""
-title="recommend_img_1"></div>
-<div class="recommend_img_2"><img src="" alt=""
-title="recommend_img_2"></div>
-<div class="recommend_img_3"><img src="" alt=""
-title="recommend_img_3"></div>
-<div class="detail"><a href="detail.html">キャンプ場詳細を見る</a></div>
-</div>
-
-
-</div>
-</div>
-
-
-</div>
-				  
-
-
-
-
-
-                  <div class="recommend_camp">
+                  <div class="recommend_wrap">
                                     <div class="nav_title"><a id="pets">ペットと一緒に過ごせるキャンプ場</a></div>
                                     <div class="nav_title_img">
                                                       <img src="images/main-img.jpg" width="320" height="250" alt="">
@@ -169,7 +186,7 @@ title="recommend_img_3"></div>
                                                                         </div>
                                                       </div>
                                     </div>
-                                    <div class="recommend_camp">
+                                    <div class="recommend_wrap">
                                                       <div class="nav_title"><a id="spa">温泉のあるキャンプ場</a></div>
                                                       <div class="nav_title_img">
                                                                         <img src="images/main-img.jpg" width="320"
@@ -237,19 +254,3 @@ title="recommend_img_3"></div>
 </body>
 
 </html>
- </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
-
