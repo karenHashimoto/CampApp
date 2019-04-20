8b650264ac9ba899da81196ecf392ccca0c128bf
KarenHashimoto <carrie4021@gmail.com>
2019-04-20 15:29:34 +0900

    再チャレンジ！

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8"/>
-    <title>camcierge</title>
+    <title>camcierge★</title>
     <meta name="viewport" content="width=device-width; initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
     <script src="vendor/jquery/jquery.min.js"></script>
