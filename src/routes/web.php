7e1c6738e58ddac22e1ffd07642ccd3351c6bc4f
yuki yamamoto <yukiyamamoto@yukinoMacBook-Pro.local>
2019-05-09 18:02:17 +0900

    web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,16 @@
 <?php
-Route::get('/', 'TopController@index');
 
-Route::get('/detail/{id}', 'InformationController@show');
+use App\Camp;
+use App\Title;
+use App\Point;
+use App\Camp_Point;
+use Illuminate\Http\Request;
+
+
+Route::get('/test_top', 'Test_TopController@index');
+
+Route::get('/detail/{id}', 'Test_DetailController@index');
+
+Route::get('/test_02/{id}', 'Test_02Controller@index');
+
+Route::resource('/photos', 'PhotosController', ['only' => ['create', 'store']]);
