7231a39aed78d86db5d2b40d060e219034f30415
yuki yamamoto <yukiyamamoto@yukinoMacBook-Pro.local>
2019-05-14 20:07:33 +0900

    camp.php変更

--- src/app/Camp.php
+++ src/app/Camp.php
@@ -13,8 +13,13 @@ class Camp extends Model
 public function points()
     {
         return $this->hasMany('App\Point');
-
-       
+        
+    }
+ 
+    public function facilities()
+    {
+        return $this->hasMany('App\Facility');
+        
     }
 
 
