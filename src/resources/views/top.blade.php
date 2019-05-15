db4c46999b8871aa3f2298531fba1666ada05947
KarenHashimoto <carrie4021@gmail.com>
2019-05-15 16:28:23 +0900

    cssを最新に更新しました。

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -314,6 +314,7 @@
         </div>
     </div>
     @endforeach
+    <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d853441.1330058764!2d130.26025588813894!3d33.32523506699939!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3541336fa8b6fb39%3A0x7c8730805bc443cb!2z44Oc44Kk44Oc44Kk44Kt44Oj44Oz44OX5aC077yI5pen77ya44Oi44O844Oi44O844Op44Oz44OJ5LmF5L2P44Kq44O844OI44Kt44Oj44Oz44OX5aC077yJ!5e0!3m2!1sja!2sjp!4v1557394017505!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
 </body>
 
 
