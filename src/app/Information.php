2144c353a6e6f8bdbdeee7343dcf5a13245c25a0
KarenHashimoto <carrie4021@gmail.com>
2019-05-16 14:01:50 +0900

    コミットします。

--- src/app/Information.php
+++ src/app/Information.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Information extends Model
 {
+    // protected $table = 'camps';
 
+    // public function scopeTwoHour($query)
+    // {
+    //     return $query->whereIn('id', [1, 2, 3,]);
+    // }
 }
--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(CampsTableSeeder::class);
+
+        $this->call(Titles_TableSeeder::class);
     }
 }
