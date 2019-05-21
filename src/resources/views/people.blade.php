04011a7302d82840a9fb931d62775357904acd77
KarenHashimoto <carrie4021@gmail.com>
2019-05-21 12:03:19 +0900

    ガッキーさんへ人数確認ページを送ります。

--- src/resources/views/people.blade.php
+++ src/resources/views/people.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>camcierge</title>
     <meta name="viewport" content="width=device-width; initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="{{ asset('css/people.css') }}"> -->
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
 </head>
 
 <body>
@@ -23,20 +23,24 @@
     <div class="cg_img"><img src="/image/camps_img/{{$camps->camp_img}}" width="200" height="150"></div>
     <div class="plan_comment">{{$camps->plan_comment}}</div>
     <div class="plan_name">{{$camps->plan_name}}</div>
-    <div class="panel-body">
 
-        <form action="{{route('people')}}" method="get">
+
+    <form action="/date/{{$camps->id}}" method="get">
         {{-- <form action="{{ route('people') }}" method="post" name="number"> --}}
-            {{ csrf_field()}}
-            <div>大人の人数</div>
-            <input type="text" name="adlut_child" id="adlut_child">
-            <div>子供の人数</div>
-            <input type="text" name="child_child" id="child_child">
-            <div>
-                <input type="hidden" value="{{$camps->id}}" name="camp_id">
+        {{ csrf_field()}}
+        <div>大人の人数</div>
+        <input type="text" name="adlut_number" id="adlut_number">
+        <div>子供の人数</div>
+        <input type="text" name="child_number" id="child_number">
+        <div>
+            <input type="submit" class="button_flat">
+
+
+
             {{-- <button type="submit"><a href="/date/{{$camps->id}}">日にちを予約</a></button></div> --}}
-                <button type="submit">日にちを予約</button></div>
-        </form>
+        {{-- <button type="submit"><a href="/date/{{$camps->id}}">日にちの確認へ進む</button> --}}
+
+    </form>
 
 
 
