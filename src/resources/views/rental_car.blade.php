f9221175cc6106d92b1ad0ac79022244ebefa01b
KarenHashimoto <carrie4021@gmail.com>
2019-05-23 15:15:25 +0900

    道具・レンタカー・最終確認のblade.phpできました。

--- src/resources/views/rental_car.blade.php
+++ src/resources/views/rental_car.blade.php
@@ -5,18 +5,77 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/header.css">
+    <link rel="stylesheet" href="/css/rentacar.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP|Roboto|Noto+Serif+JP" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Gaegu" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     <title>Document</title>
 </head>
 
 <body>
+    <div class="fixed">
+        <div class="header">
+            <div class="before_button">
+                <a href="◯◯" class="button_color_glay"><i class="material-icons large-size">
+                        navigate_before
+                    </i> </a>
+            </div>
+            <div class="logo_area">
+                <a class="logo" href="index.html"><img src="/image/logo.svg"></a>
+            </div>
+            <div class="next_button">
+            </div>
 
+        </div>
+    </div>
+    <div class="reserve_band">一括予約</div>
+    <div class="stepper_wrap">
+        <div class="stepper">
+            <div class="circle">
+                <div class="circle-inner">80%</div>
+            </div>
+        </div>
+        <div class="purpose_unit">
+            <div class="purpose">レンタカー予約</div>
+            <div class="purpose_next">予約完了まで あとすこし♫</div>
+        </div>
+    </div>
+    <div class="rental_image">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTLShsgDnFDj7H6DJt4oFPIb_gMce2GMx_KQJAqEQoh3A4nfj3_">
 
-    <div>レンタカーは必要ですか？</div>
+    </div>
     <form action="{{url('/confirm/' )}}" method="post">
         {{ csrf_field()}}
-        <input type="submit" class="button_flat" value="はい" name="car_yes">
-        <input type="submit" class="button_flat" value="いいえ" name="car_no">
+        <div class="button_wrap_wide">
+            <div><button class="button_flat_wide" value="はい" name="car_yes">
 
+                    <div class="before_button">
+                    </div>
+
+                    <div class="center_button">レンタカー予約する</div>
+
+                    <div class="next_button">
+                        <a href="" class="button_color_white"><i class="material-icons large-size">
+                                navigate_next
+                            </i> </a>
+                    </div>
+                </button></div>
+        </div>
+        <div class="button_wrap_wide">
+            <div><button class="button_flat_wide_white" value="いいえ" name="car_no">
+
+                    <div class="before_button">
+                    </div>
+
+                    <div class="center_button">しない</div>
+
+                    <div class="next_button">
+                    </div>
+                </button></div>
+        </div>
         <input type="hidden" name="camp_id" value="{{$camps->id}}">
         <input type="hidden" name="adlut_number" value="{{$adlut_number}}">
         <input type="hidden" name="child_number" value="{{$child_number}}">
--- src/resources/views/rental_item.blade.php
+++ src/resources/views/rental_item.blade.php
@@ -1,33 +1,92 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/header.css">
+    <link rel="stylesheet" href="/css/rental.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP|Roboto|Noto+Serif+JP" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Gaegu" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="http://maps.google.com/maps/api/js?key=AIzaSyBunFa8M5cUXVfE2ewS5ZCueUIh-AxtB5Y&language=ja"></script>
     <title>Document</title>
 </head>
 
 <body>
-    <div>道具の予約いりますか？</div>
+    <div class="fixed">
+        <div class="header">
+            <div class="before_button">
+                <a href="○○" class="button_color_glay"><i class="material-icons large-size">
+                        navigate_before
+                    </i> </a>
+            </div>
+            <div class="logo_area">
+                <a class="logo" href="/"><img src="/image/logo.svg"></a>
+            </div>
+            <div class="next_button">
+            </div>
+
+        </div>
+    </div>
+    <div class="reserve_band">一括予約</div>
+    <div class="stepper_wrap">
+        <div class="stepper">
+            <div class="circle">
+                <div class="circle-inner">60%</div>
+            </div>
+        </div>
+        <div class="purpose_unit">
+            <div class="purpose">アイテムレンタル</div>
+            <div class="purpose_next">次はレンタカー選択</div>
+        </div>
+    </div>
+
+
+    <div class="rental_image">
+        <img src="https://shop12-makeshop.akamaized.net/shopimages/outdoorlife/000000000019.jpg">
+
+    </div>
+
 
     <form action="{{url('/rental_car/' )}}" method="post">
         {{ csrf_field()}}
-        <input type="submit" class="button_flat" value="はい" name="item_yes">
-        <input type="submit" class="button_flat" value="いいえ" name="item_no">
+        <div class="button_wrap_wide">
+            <div>
+                <button class="button_flat_wide" name="item_yes" input type="submit">
+                    <div class="before_button"></div>
+
+                    <div class="center_button">レンタルする</div>
+                    <div class="next_button">
+                        <a href="◯◯" class="button_color_white"><i class="material-icons large-size">
+                                navigate_next
+                            </i> </a>
+                    </div>
+                </button></div>
+        </div>
+
+        </div>
+        <div class="button_wrap_wide">
+            <div><button class="button_flat_wide_white" name="item_no" input type="submit">
+
+                    <div class="before_button">
+                    </div>
+                    <div class="center_button">しない</div>
+                    <div class="next_button">
+                    </div>
+                </button>
+            </div>
+        </div>
+
         <input type="hidden" name="camp_id" value="{{$camps->id}}">
         <input type="hidden" name="adlut_number" value="{{$adlut_number}}">
         <input type="hidden" name="child_number" value="{{$child_number}}">
         <input type="hidden" name="inDate" value="{{$inDate}}">
         <input type="hidden" name="outDate" value="{{$outDate}}">
 
-
     </form>
-    <div>大人：{{$adlut_number}}人</div>
-    <div>小人：{{$child_number}}人</div>
-    <div>チェックイン：{{$inDate}}</div>
-    <div>チェックアウト：{{$outDate}}</div>
-
 </body>
 
 </html>
