4ed614324ca0cd364184f97443fe55465d311179
yuki yamamoto <yukiyamamoto@yukinoMacBook-Pro.local>
2019-05-28 17:11:49 +0900

    recommend_index編集後

--- src/app/Http/Controllers/CampsController.php
+++ src/app/Http/Controllers/CampsController.php
@@ -34,7 +34,7 @@ class CampsController extends Controller
         return view('top', ['camps1' => $camps1, 'camps2' => $camps2, 'camps3' => $camps3, 'titles1' => $titles1, 'titles2' => $titles2, 'titles3' => $titles3]);
     }
 
-    public function recomend_index(Request $request){
+    public function recommend_index(Request $request){
 
         $camps1 = Camp::whereIn('id', [1, 2, 3])->get(); //collection_object
         $titles1 = Title::find(1);
