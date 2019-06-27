567d5dbd88925c39da3a245d64aed946379ecb1c
yuki yamamoto <yukiyamamoto@yukinoMacBook-Pro.local>
2019-06-27 20:05:54 +0900

    GA

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -2,6 +2,18 @@
 <html lang="ja">
 
 <head>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-142770356-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-142770356-1');
+  </script>
+
+
   <meta charset="utf-8" />
   <title>camcierge</title>
 
--- src/resources/views/people.blade.php
+++ src/resources/views/people.blade.php
@@ -1,6 +1,18 @@
 <html lang="ja">
 
 <head>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-142770356-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-142770356-1');
+    </script>
+
+
     <meta charset="utf-8">
     <title>camcierge</title>
     <meta name="viewport" content="width=device-width; initial-scale=1.0">
